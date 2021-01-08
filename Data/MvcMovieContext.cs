219ba493a4cb61f5a516b39a20eee5b75637e10a
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-01-08 18:07:12 -0300

    add console logs

--- Data/MvcMovieContext.cs
+++ Data/MvcMovieContext.cs
@@ -1,15 +1,25 @@
+using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MvcMovie.Models;
 
 namespace MvcMovie.Data
 {
     public class MvcMovieContext : DbContext
     {
-        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+        public IConfiguration Configuration { get; }
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
             : base(options)
         {
         }
 
         public DbSet<Movie> Movie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
     }
 }
